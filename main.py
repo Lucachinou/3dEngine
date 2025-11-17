248855aab4fddddcb15c5655bce927dbbb7983b6
lucachinou <lucachinou@duck.com>
2025-11-17 10:36:23 +0100

    Fixed collision system + Finalized refactoring of camera and feet position

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ Player = {
     'WorldInteraction': {
         'speed': 0.002,
         'gravity': 0.04,
-        'jump_strengh': 0.5,
+        'friction': 0.04,
+        'jump_strengh': 0.35,
         'velocity': [0.0, 0.0, 0.0],
     },
     'CameraRelative': {
@@ -21,7 +22,7 @@ Player = {
     'PlayerRelative': {
         'Scale': 1.0,
         'FeetPosition': [0.0, 0.0, 0.0],
-        'on_ground': False,
+        'on_ground': True,
     },
     'Settings': {
         'ActiveKeys': set(),
@@ -190,22 +191,25 @@ def resolve_collision(player_pos, player_half, cube_pos, cube_half):
     dy = py - cy
     dz = pz - cz
 
-    overlap_x = (cube_half[0]) - (abs(dx) + 0.3)
-    overlap_y = (player_half[1] + cube_half[1]) - abs(dy)
-    overlap_z = (cube_half[2]) - (abs(dz) + 0.3)
+    overlap_x = (player_half[0] + cube_half[0]) - abs(dx)
+    overlap_y = (player_half[1] + cube_half[1]) - abs(dy) + 1.0
+    overlap_z = (player_half[2] + cube_half[2]) - abs(dz)
 
     if overlap_x > 0 and overlap_y > 0 and overlap_z > 0:
         if dy > 0 and overlap_y < overlap_x and overlap_y < overlap_z:
-            py += cy + cube_half[1] + player_half[1]
+            # TODO: Use only player_half and cube_half instead of hardcoded threshold
             Player['PlayerRelative']['on_ground'] = True
             Player['WorldInteraction']['velocity'][1] = 0
+            py += cube_half[1] + player_half[1] - 1.0
             return [px, py, pz]
-        if overlap_x < overlap_y and overlap_x < overlap_z:
+        if overlap_x < overlap_y and overlap_x < overlap_z and py <= (cy + 1.0):
             px += overlap_x * (1 if dx > 0 else -1)
-        elif overlap_y < overlap_z:
+        elif overlap_y < overlap_z and py <= (cy + 1.0):
             py += overlap_y * (1 if dy > 0 else -1)
-        elif overlap_z < overlap_x:
+        elif overlap_z < overlap_x and py <= (cy + 1.0):
             pz += overlap_z * (1 if dz > 0 else -1)
+        else:
+            Player['PlayerRelative']['on_ground'] = False
     return [px, py, pz]
 
 def display():
@@ -227,24 +231,36 @@ def display():
     if Player['PlayerRelative']['on_ground'] == False:
         Player['WorldInteraction']['velocity'][1] -= Player['WorldInteraction']['gravity'] * dt * 60
 
-    Player['CameraRelative']['CameraPosition'][1] += Player['WorldInteraction']['velocity'][1] * dt * 60
+    for i in [0, 2]:
+        if Player['WorldInteraction']['velocity'][i] > 0:
+            Player['WorldInteraction']['velocity'][i] -= Player['WorldInteraction']['friction'] * dt * 60
+            if Player['WorldInteraction']['velocity'][i] < 0:
+                Player['WorldInteraction']['velocity'][i] = 0
+        elif Player['WorldInteraction']['velocity'][i] < 0:
+            Player['WorldInteraction']['velocity'][i] -= Player['WorldInteraction']['friction'] * dt * 60
+            if Player['WorldInteraction']['velocity'][i] > 0:
+                Player['WorldInteraction']['velocity'][i] = 0
+
+    Player['PlayerRelative']['FeetPosition'][0] += Player['WorldInteraction']['velocity'][0] * dt * 60
+    Player['PlayerRelative']['FeetPosition'][1] += Player['WorldInteraction']['velocity'][1] * dt * 60
+    Player['PlayerRelative']['FeetPosition'][2] += Player['WorldInteraction']['velocity'][2] * dt * 60
 
     for element in WorldElements:
         draw_cube(element['position'][0], element['position'][1], element['position'][2])
         Player['PlayerRelative']['FeetPosition'] = resolve_collision(
             Player['PlayerRelative']['FeetPosition'],
-            [0.25, 0.9, 0.25],
+            [0.25, 0.5, 0.25],
             [element['position'][0], element['position'][1], element['position'][2]],
-            element['size']
+            [(element['size'][0] / 2), (element['size'][1] / 2), (element['size'][2] / 2)]
         )
 
     if Player['PlayerRelative']['FeetPosition'][1] <= 0.0:
-        Player['PlayerRelative']['FeetPosition'][1] = 0.0
         Player['WorldInteraction']['velocity'][1] = 0.0
         Player['PlayerRelative']['on_ground'] = True
 
     keyboard()
     update_camera()
+
     glutSwapBuffers()
 
 def update_camera():
@@ -270,6 +286,9 @@ def keyboard():
     if 'q' in Player['Settings']['ActiveKeys']:
         Player['PlayerRelative']['FeetPosition'][0] -= right[0] * Player['WorldInteraction']['speed']
         Player['PlayerRelative']['FeetPosition'][2] -= right[2] * Player['WorldInteraction']['speed']
+    if 'v' in Player['Settings']['ActiveKeys']: # TODO: Delete this when all velocity test are finished!
+        print("Applying velocity..")
+        Player['WorldInteraction']['velocity'][1] += 0.1
     if ' ' in Player['Settings']['ActiveKeys']:
         if Player['PlayerRelative']['on_ground']:
             Player['WorldInteraction']['velocity'][1] = Player['WorldInteraction']['jump_strengh']
@@ -279,7 +298,7 @@ def mouse_click(button, state, x, y):
     forward = get_camera_forward()
     if button == GLUT_LEFT_BUTTON and state == GLUT_DOWN:
         WorldElements.append({
-            'position': [int(Player['CameraRelative']['CameraPosition'][0] - forward[0] * 3), int(Player['CameraRelative']['CameraPosition'][1]  - forward[1] * 3), int(Player['PlayerRelative']['CameraRelative'][2] - forward[2] * 3)],
+            'position': [int(Player['CameraRelative']['CameraPosition'][0] - forward[0] * 3), int(Player['CameraRelative']['CameraPosition'][1]  - forward[1] * 3), int(Player['CameraRelative']['CameraPosition'][2] - forward[2] * 3)],
             'size': [1.0, 1.0, 1.0],
         })
     elif button == GLUT_RIGHT_BUTTON and state == GLUT_DOWN:
